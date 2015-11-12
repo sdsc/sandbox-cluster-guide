233d2a82ee89aa67c50c4750d06e9b11030d16ab
cjy008 <cindy.yu03@gmail.com>
2015-11-12 08:25:20 -0800

    Update images.py
    Displays image in 4 windows on linux machine right now. Still have to work on some bugs.

---
+++ examples/images.py
@@ -0,0 +1,47 @@
+import pygame
+import time
+from mpi4py import MPI
+
+img = pygame.image.load('20140902_200423.jpg')
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+In = 1
+pygame.display.init()
+
+white = (255,255,255)
+w = img.get_rect().width
+h = img.get_rect().height
+
+print w, ",", h
+
+halfWidth = w/2
+halfHeight = h/2
+
+print halfWidth, ",", halfHeight
+
+size=(halfWidth, halfHeight)
+screen = pygame.display.set_mode((size))
+screen.fill((white))
+rect0 = pygame.Rect(0,0,halfWidth,halfHeight)
+rect1 = pygame.Rect(halfWidth, 0, halfWidth, halfHeight)
+rect2 = pygame.Rect(0, halfHeight, halfWidth, halfHeight)
+rect3 = pygame.Rect(halfWidth, halfHeight, halfWidth, halfHeight)
+#screen0.fill((white))
+pygame.display.get_init
+#pygame.display.update()
+
+while In:
+	screen.fill((white))
+	if comm.rank == 0:
+		screen.blit(img, (0,0),rect0)
+		pygame.display.update()
+	elif comm.rank == 1:
+		screen.blit(img, (0,0), rect1)
+		pygame.display.update()
+	elif comm.rank == 2:
+		screen.blit(img, (0,0), rect2)
+		pygame.display.update()
+	else:
+		screen.blit(img, (0,0), rect3)
+		pygame.display.update()
