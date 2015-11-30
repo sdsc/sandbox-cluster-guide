55fdaeee41ccc513ab8119b29c7fddd639b17360
Richard Wagner <rpwagner@Colossus.local>
2015-11-30 00:01:12 -0800

    basic cluster config using initial dhcp

---
+++ config/README.md
@@ -0,0 +1,5 @@
+= Manual Steps =
+
+ * raspi-config expand file system
+ * On pinode-0 generate RSA key pair for cluster
+ 
---
+++ config/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+    echo "Must supply node number."
+    echo "Example: ./deploy.sh 1"
+    exit -1
+fi
+
+nodedir=$1
+
+sudo apt-get install emacs openmpi-bin libopenmpi-dev python-mpi4py -y
+if [ $nodedir -eq 0 ]
+then
+    cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+    sudo apt-get install nfs-kernel-server rpcbind isc-dhcp-server -y
+    for i in 2 3 4 5
+    do
+        sudo mv rc$i.d/S01nfs-kernel-server rc$i.d/S02nfs-kernel-server
+        sudo ln -s init.d/rpcbind rc$i.d/S01rpcbind
+    done
+fi
+
+sudo cp -rf $nodedir/* /etc/
+sudo cp -rf common/* /etc/
+
+
