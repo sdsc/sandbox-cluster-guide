796bae3cbae62c0a11ffb574c0f97ce52f851681
cjy008 <cindy.yu03@gmail.com>
2015-12-01 13:22:06 -0800

    Update turtle.py
    Added comments

--- examples/turtle.py
+++ examples/turtle.py
@@ -1,10 +1,12 @@
 from turtle import *
+
 setup(500, 500)
 Screen()
 title("Turtle Keys")
 move = Turtle()
-showturtle()
+#showturtle()
 
+#Functions for the keyboard input
 def k1():
     move.forward(45)
 
@@ -17,10 +19,13 @@ def k3():
 def k4():
     move.back(45)
 
+#Calls the functions when it detects a certain keyboard input
 onkey(k1, "Up")
 onkey(k2, "Left")
 onkey(k3, "Right")
 onkey(k4, "Down")
 
+#Program listens for the keyboard inputs
 listen()
+#Mainloop prevents the program from closing the window itself
 mainloop()
