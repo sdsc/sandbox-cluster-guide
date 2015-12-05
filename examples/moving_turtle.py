49267c1f6ff40e20d937741a803d342d34b0fa3d
cjy008 <cindy.yu03@gmail.com>
2015-12-05 07:56:20 -0800

    Update moving_turtle.py

--- examples/moving_turtle.py
+++ examples/moving_turtle.py
@@ -1,65 +1,59 @@
-from turtle import *
 import pygame
-from mpi4py import MPI
 
-img = pygame.image.load('gingerbreadman.jpg')
+pygame.init()
+
+display_width = 800
+display_height = 800
+
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption("TEST")
+
+black = (0,0,0)
 white = (255,255,255)
-w = img.get_rect().width
-h = img.get_rect().height
-size=(w,h)
-halfWidth = w/2
-halfHeight = h/2
-screen = pygame.display.set_mode((halfWidth,halfHeight))
-screen.fill((white))
-screen.blit(img, (0,0))
 
-pygame.display.update()
-#title("Turtle Keys")
-pygame.display.get_init
+clock = pygame.time.Clock()
+crashed = False
+image = pygame.image.load('turtle.png')
 
-move =Turtle()
-#showturtle()
-move.goto(100,100)
-move.shape('turtle')
+def turtle(x,y):
+	gameDisplay.blit(image, (x,y))
 
-def k1():
-    move.forward(45)
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+x_change = 0
+y_change = 0
+running = 1
 
-def k2():
-    move.left(45)
+while not crashed:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			crashed = True
 
-def k3():
-    move.right(45)
+	if event.type == pygame.KEYDOWN:
+		if event.key == pygame.K_LEFT:
+			x_change = -5
+		elif event.key == pygame.K_RIGHT:
+			x_change = 5
+		elif event.key == pygame.K_UP:
+			y_change = -5
+		elif event.key == pygame.K_DOWN:
+			y_change = 5
+	if event.type == pygame.KEYUP:
+			y_change = 0
+			x_change = 0
 
-def k4():
-    move.back(45)
+	x += x_change
+	y += y_change
+	pygame.display.update()
 
-onkey(k1, "Up")
-onkey(k2, "Left")
-onkey(k3, "Right")
-onkey(k4, "Down")
+	gameDisplay.fill(white)
+	turtle(x,y)
 
-In = 1
-rect0 = pygame.Rect(0,0,halfWidth,halfHeight)
-rect1 = pygame.Rect(halfWidth, 0, halfWidth, halfHeight)
-rect2 = pygame.Rect(0, halfHeight, halfWidth, halfHeight)
-rect3 = pygame.Rect(halfWidth, halfHeight, halfWidth, halfHeight)
-while In:
-	screen.fill((white))
-	if comm.rank == 0:
-		screen.blit(img, (0,0),rect0)
-		pygame.display.update()
-	elif comm.rank == 1:
-		screen.blit(img, (0,0), rect1)
-		pygame.display.update()
-	elif comm.rank == 2:
-		screen.blit(img, (0,0), rect2)
-		pygame.display.update()
-	else:
-		screen.blit(img, (0,0), rect3)
-		pygame.display.update()
+	pygame.display.update()
+	clock.tick(60)
 
-listen()
-mainloop()
+pygame.quit()
+quit()
