495fa9812110f3e21a3d6e9f87c5d1f5da781641
Cindy Yu <cindy.yu03@gmail.com>
2016-02-29 01:13:41 -0800

    Finished third graph

--- examples/analyze_data.py
+++ examples/analyze_data.py
@@ -26,31 +26,57 @@ for d in csv.DictReader(open('/home/hungmaau2/sandbox-cluster-guide/examples/sle
 	days.append(int(d['Days']))
 	subject.append(int(d['Subject']))
 
-if comm.rank == 0:
-	selected_subject = subject[0]
-	subjects = [selected_subject]
-	compiled_list = []
-	lists = []
+selected_subject = subject[0]
+subjects = [selected_subject]
+compiled_list = []
+lists = []
+i = 0
+for sub in subject:
+	if sub == selected_subject:
+		lists.append(reaction[i])
+	else:
+		compiled_list.append(lists)
+		subjects.append(sub)
+		selected_subject = sub
+		lists = []
+		lists.append(reaction[i])
+	i+=1	
+compiled_list.append(lists)
+
+set_days = []
+for day in days:
+	if day in set_days:
+		continue
+	else:
+		set_days.append(day)
+
+hashmap = []
+selected_day = days[0]
+compiled_reaction_by_day = []
+for day_t in set_days:
+	selected_day = day_t
+	temp_list = []
 	i = 0
-	for sub in subject:
-		if sub == selected_subject:
-			lists.append(reaction[i])
-		else:
-			compiled_list.append(lists)
-			subjects.append(sub)
-			selected_subject = sub
-			lists = []
-			lists.append(reaction[i])
-		i+=1	
-	compiled_list.append(lists)	
-	
-	#print compiled_list
-	set_days = []
 	for day in days:
-		if day in set_days:
-			continue
-		else:
-			set_days.append(day)
+		if day == selected_day:
+			temp_list.append(reaction[i])
+		i+=1
+			
+	
+	compiled_reaction_by_day.append(temp_list)
+#compiled_reaction_by_day.append(temp_list)
+
+#print compiled_reaction_by_day[0]
+
+reaction_sum_list = []
+for n in compiled_reaction_by_day:
+	reaction_sum_list.append(sum(n)/len(n))
+
+print reaction_sum_list		
+
+#print compiled_list
+
+if comm.rank == 0:
 	#print set_days
 	#print subjects
 	mpl.figure()
@@ -58,12 +84,20 @@ if comm.rank == 0:
 	while i < len(subjects):
 		mpl.plot(set_days,compiled_list[i])
 		i+=1
+	mpl.ylabel('Reaction times')
+	mpl.xlabel('Number of sleep deprived days')
 	mpl.show()
 
 if comm.rank == 1:
 	#plot of reaction time over days
 	mpl.plot(days,reaction,'ro')
 	mpl.ylabel('Reaction times')
+	mpl.xlabel('Number of sleep deprived days')
+	mpl.show()
+
+if comm.rank == 2:
+	mpl.plot(set_days,reaction_sum_list)
+	mpl.ylabel('Average reaction times of all subjects')
 	mpl.xlabel('Number of sleep deprived days')
 	mpl.show()
 
